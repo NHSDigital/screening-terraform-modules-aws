f809d1f2a571f1511d25010e7ccfce3130d2d5a8
Andrew <andrew.cleveland1@nhs.net>
2025-07-25 12:03:15 +0100

    AC - removed old terraform_modules

--- terraform_modules/vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "environment" {
-  description = "The name of the Environment this is deployed into, for example CICD, NFT, UAT or PROD"
-}
-
-variable "name" {
-  description = "The name of the resource"
-}
