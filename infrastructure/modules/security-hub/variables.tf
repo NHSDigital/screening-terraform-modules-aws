9c399c6143ec65f1f7ee254f8267b454dee7927c
Andrew <andrew.cleveland1@nhs.net>
2025-08-06 10:28:38 +0100

    AC - updated bucket reference

--- infrastructure/modules/security-hub/variables.tf
+++ infrastructure/modules/security-hub/variables.tf
@@ -6,31 +6,10 @@ variable "name_prefix" {
 variable "name" {
   description = "The name of the resource"
   type        = string
-  default     = "-elasticache"
+  default     = "security-hub"
 }
 
-variable "environment" {
-  description = "The name of the Environment this is deployed into, for example CICD, NFT, UAT or PROD"
-  type        = string
-}
-
-variable "aws_account_id" {
-  description = "The AWS account ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "vpc_id" {
-  description = "The ID for the VPC"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "The subnets that will be used for elasticache, usually private"
-  type        = list(string)
-}
-
-variable "s3_bucket_name" {
+variable "s3_bucket" {
   description = "The s3 bucket that security-hub will use"
   type        = string
 }
