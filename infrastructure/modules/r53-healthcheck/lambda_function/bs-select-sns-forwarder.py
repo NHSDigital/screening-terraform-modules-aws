6426d2292338925ecceaa4e088d7b6fa98c9b581
Andrew <andrew.cleveland1@nhs.net>
2026-05-05 10:12:58 +0100

    AC - updated formatting

--- infrastructure/modules/r53-healthcheck/lambda_function/bs-select-sns-forwarder.py
+++ infrastructure/modules/r53-healthcheck/lambda_function/bs-select-sns-forwarder.py
@@ -5,11 +5,10 @@ import os
 from resources.sns_forwarder import SNSForwarder
 
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
     logging.getLogger().setLevel(logging.INFO)
     logging.info("event : {}".format(event))
-    sns_client = boto3.client('sns',region_name='eu-west-2')
-    target_topic_arn=os.environ["EU_WEST_2_SNS"]
+    sns_client = boto3.client("sns", region_name="eu-west-2")
+    target_topic_arn = os.environ["EU_WEST_2_SNS"]
     sns_forwarder = SNSForwarder(sns_client, target_topic_arn)
-    return sns_forwarder.forward_message(event['Records'][0]['Sns']['Message'])
-
+    return sns_forwarder.forward_message(event["Records"][0]["Sns"]["Message"])
