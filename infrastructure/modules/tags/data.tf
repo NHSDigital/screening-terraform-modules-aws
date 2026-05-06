62d5da9fca144b9995f12b169fc8d41f882561d6
Oliver Slater <oliver.slater2@nhs.net>
2026-05-06 15:47:23 +0100

    style(infrastructure/modules/tags): lint fix and formatting

--- infrastructure/modules/tags/data.tf
+++ infrastructure/modules/tags/data.tf
@@ -5,5 +5,5 @@ data "aws_region" "current" {}
 data "aws_iam_session_context" "current" {
   # This data source provides information on the source
   # IAM role when an assumed role is utilized
- arn = data.aws_caller_identity.current.arn
+  arn = data.aws_caller_identity.current.arn
 }
