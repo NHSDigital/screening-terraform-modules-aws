7a940936385eb8bb67c929ab3c51b939288d9c11
Oliver Slater <oliver.slater2@nhs.net>
2026-05-27 19:12:50 +0100

    feat(release): update module source version to v2.1.0 across inspector and kms modules

--- infrastructure/modules/tags/exports/context.tf
+++ infrastructure/modules/tags/exports/context.tf
@@ -21,7 +21,7 @@
 #
 
 module "this" {
-  source = "git::https://github.com/NHSDigital/screening-terraform-modules-aws.git//infrastructure/modules/tags?ref=v2.0.0"
+  source = "git::https://github.com/NHSDigital/screening-terraform-modules-aws.git//infrastructure/modules/tags?ref=v2.1.0"
 
   service             = var.service
   project             = var.project
