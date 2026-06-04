885d28d1946fb062adca4de42e69612ff3910bd4
Dave Hinton <dave.hinton1@nhs.net>
2026-06-04 08:21:14 +0100

    initial acm instance

--- infrastructure/modules/acm/main.tf
+++ infrastructure/modules/acm/main.tf
@@ -1,1 +1,30 @@
 # DAVEH
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 6.3.0"
+
+  acm_certificate_domain_validation_options = DAVEH
+  certificate_transparency_logging_preference = DAVEH
+  create_certificate = DAVEH
+  create_route53_records = DAVEH
+  create_route53_records_only = DAVEH
+  distinct_domain_names = DAVEH
+  dns_ttl = DAVEH
+  domain_name = DAVEH
+  export = DAVEH
+  key_algorithm = DAVEH
+  private_authority_arn = DAVEH
+  region = DAVEH
+  subject_alternative_names = DAVEH
+  tags = module.this.tags
+  validate_certificate = DAVEH
+  validation_allow_overwrite_records = DAVEH
+  validation_method = DAVEH
+  validation_option = DAVEH
+  validation_record_fqdns = DAVEH
+  validation_timeout = DAVEH
+  wait_for_validation = DAVEH
+  zone_id = DAVEH
+  zones = DAVEH
+}
