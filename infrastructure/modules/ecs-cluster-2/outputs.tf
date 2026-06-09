7d8c3e6d887b026d50339d314e7c5ad9635aec41
Dave Hinton <dave.hinton1@nhs.net>
2026-06-09 14:38:13 +0100

    add outputs we will need

--- infrastructure/modules/ecs-cluster-2/outputs.tf
+++ infrastructure/modules/ecs-cluster-2/outputs.tf
@@ -1,1 +1,9 @@
-# DAVEH
+output "cluster_arn" {
+  description = "ARN that identifies the cluster"
+  value       = module.ecs_cluster.arn
+}
+
+output "cluster_name" {
+  description = "Name that identifies the cluster"
+  value       = module.ecs_cluster.name
+}
