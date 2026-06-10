9a9fe9473eb1d63c73baa419af1dcd9c01f61a1a
Dave Hinton <dave.hinton1@nhs.net>
2026-06-10 15:41:53 +0100

    note underlying ec2_instance fields we currently set

--- infrastructure/modules/ec2-instance/main.tf
+++ infrastructure/modules/ec2-instance/main.tf
@@ -6,5 +6,38 @@ module "ec2_instance" {
   name   = module.this.id
   tags   = module.this.tags
 
+  ami                     = DAVEH
+  instance_type           = DAVEH
+  key_name                = DAVEH
+  disable_api_termination = DAVEH
+  ebs_optimized           = DAVEH
+  monitoring              = true
+  subnet_id               = DAVEH
+
+  vpc_security_group_ids = DAVEH
+
+  iam_instance_profile = DAVEH
+
+  # root_block_device {
+  #   encrypted   = true
+  #   kms_key_id  = data.terraform_remote_state.shared_resources.outputs.oracle_ebs_snapshot_kms_key_arn
+  #   volume_size = var.oracle19_root_volume_size
+  #   volume_type = "gp3"
+  # }
+
+  # metadata_options {
+  #   http_tokens                 = "required"
+  #   http_endpoint               = "enabled"
+  #   http_put_response_hop_limit = 1
+  #   instance_metadata_tags      = "disabled"
+  # }
+
+  user_data                   = DAVEH
+  user_data_replace_on_change = false
+
+  # lifecycle {
+  #   ignore_changes = [user_data]
+  # }
+
   # DAVEH
 }
