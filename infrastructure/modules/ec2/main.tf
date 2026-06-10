2264133998cf74e4ee812ad4d355d4421d8efc8a
Dave Hinton <dave.hinton1@nhs.net>
2026-06-10 14:34:00 +0100

    module bare bones

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -1,1 +1,10 @@
-# DAVEH
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "6.4.0"
+
+  create = module.this.enabled
+  name   = module.this.id
+  tags   = module.this.tags
+
+  # DAVEH
+}
