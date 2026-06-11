450e7ac0ec1f6a1f7092e6ddf001b1db1f834569
Dave Hinton <dave.hinton1@nhs.net>
2026-06-11 15:19:48 +0100

    add outputs used in existing stack

--- infrastructure/modules/ec2-instance/outputs.tf
+++ infrastructure/modules/ec2-instance/outputs.tf
@@ -1,1 +1,9 @@
-# DAVEH
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = module.ec2_instance.id
+}
+
+output "private_ip" {
+  description = "The private IP address of the EC2 instance"
+  value       = module.ec2_instance.private_ip
+}
