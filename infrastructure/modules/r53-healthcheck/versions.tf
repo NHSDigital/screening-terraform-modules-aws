e02070d335367433b53aba8b35fbd0d3828ca1a4
Oliver Slater <oliver.slater2@nhs.net>
2026-06-16 00:32:43 +0100

    feat(terraform): update required version and provider version in r53-healthcheck module

--- infrastructure/modules/r53-healthcheck/versions.tf
+++ infrastructure/modules/r53-healthcheck/versions.tf
@@ -1,7 +1,10 @@
 terraform {
+  required_version = ">= 1.13"
+
   required_providers {
     aws = {
       source                = "hashicorp/aws"
+      version               = ">= 6.42"
       configuration_aliases = [aws.us_east_1]
     }
   }
