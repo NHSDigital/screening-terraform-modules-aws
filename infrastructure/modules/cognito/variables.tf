832982f1110c4c310e20a29e38a2914e59f731fa
Oliver Slater <oliver.slater2@nhs.net>
2026-06-16 01:07:15 +0100

    feat(cognito): add variable descriptions and create versions.tf for provider requirements

--- infrastructure/modules/cognito/variables.tf
+++ infrastructure/modules/cognito/variables.tf
@@ -7,6 +7,7 @@ variable "name_prefix" {
   type        = string
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "environment" {
   description = "The name of the Environment this is deployed into, for example CICD, NFT, UAT or PROD"
   type        = string
@@ -16,35 +17,51 @@ variable "environment" {
 # COGNITO
 ##################################################################################
 variable "deletion_protection" {
+  description = "Whether user pool deletion protection is enabled"
+  type        = string
   default = "INACTIVE"
 }
 
 variable "mfa_configuration" {
+  description = "MFA mode for the user pool"
+  type        = string
   default = "OFF"
 }
 
 variable "attribute_names" {
+  description = "Cognito custom attributes to create"
   type    = list(string)
   default = ["acr", "amr", "email", "idassurancelevel", "nhsid_nrbac_roles", "bss_username", "sid", "uid"]
 }
 
 variable "message_action" {
+  description = "Message action used when creating users"
+  type        = string
   default = "SUPPRESS"
 }
 
 variable "acr" {
+  description = "Default ACR value for user attributes"
+  type        = string
   default = "AAL1_USERPASS"
 }
 
 variable "amr" {
+  description = "Default AMR value for user attributes"
+  type        = string
   default = "USERPASS"
 }
 
 variable "user_email" {
+  description = "Initial user email address"
+  type        = string
   default = "nhsdigital.axe@nhs.net"
 }
 
+# tflint-ignore: terraform_unused_declarations
 variable "user_password" {
+  description = "Initial user password placeholder"
+  type        = string
   default = "changeme"
 }
 
