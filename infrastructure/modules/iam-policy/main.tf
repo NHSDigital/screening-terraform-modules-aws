864092fa251b96def165d1212767a203d34d66c6
Dave Hinton <dave.hinton1@nhs.net>
2026-06-17 11:25:25 +0100

    feat(iam-policy): add and use context

--- infrastructure/modules/iam-policy/main.tf
+++ infrastructure/modules/iam-policy/main.tf
@@ -3,4 +3,8 @@
 module "iam_policy" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "6.6.1"
+
+  create = module.this.enabled
+  name   = module.this.id
+  tags   = module.this.tags
 }
