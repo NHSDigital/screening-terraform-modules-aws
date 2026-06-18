d3005b5c315c62015351b58bdc91fd820a755ed0
Dave Hinton <dave.hinton1@nhs.net>
2026-06-18 09:00:11 +0100

    feat(ssm-parameter): add context

--- infrastructure/modules/ssm-parameter/main.tf
+++ infrastructure/modules/ssm-parameter/main.tf
@@ -1,4 +1,8 @@
 module "ssm_parameter" {
   source  = "terraform-aws-modules/ssm-parameter/aws"
   version = "~> 2.1.0"
+
+  create = module.this.enabled
+  name   = module.this.name
+  tags   = module.this.tags
 }
