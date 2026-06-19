d135a848436892eebca97775f331450a00537b93
Oliver Slater <oliver.slater2@nhs.net>
2026-06-19 15:35:15 +0100

    refactor: update R53 module context to ignore tflint for pinned source

--- infrastructure/modules/r53/context.tf
+++ infrastructure/modules/r53/context.tf
@@ -1,3 +1,4 @@
+# tflint-ignore-file: terraform_standard_module_structure, terraform_unused_declarations
 #
 # ONLY EDIT THIS FILE IN github.com/NHSDigital/screening-terraform-modules-aws/infrastructure/modules/tags
 # All other instances of this file should be a copy of that one
@@ -21,6 +22,7 @@
 #
 
 module "this" {
+  # tflint-ignore: terraform_module_pinned_source
   source = "../tags"
 
   enabled             = var.enabled
