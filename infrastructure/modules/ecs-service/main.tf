78a1a038c930be9281de290ef01dd2603ef95b9a
Dave Hinton <dave.hinton1@nhs.net>
2026-06-22 09:09:18 +0100

    feat(ecs-service): add inputs cr

--- infrastructure/modules/ecs-service/main.tf
+++ infrastructure/modules/ecs-service/main.tf
@@ -6,16 +6,24 @@ module "ecs_service" {
   name   = module.this.name
   tags   = module.this.tags
 
-  alarms                        = var.alarms
-  assign_public_ip              = var.assign_public_ip
-  autoscaling_max_capacity      = var.autoscaling_max_capacity
-  autoscaling_min_capacity      = var.autoscaling_min_capacity
-  autoscaling_policies          = var.autoscaling_policies
-  autoscaling_scheduled_actions = var.autoscaling_scheduled_actions
-  autoscaling_suspended_state   = var.autoscaling_suspended_state
-  availability_zone_rebalancing = var.availability_zone_rebalancing
-  capacity_provider_strategy    = var.capacity_provider_strategy
-  cluster_arn                   = var.cluster_arn
-  container_definitions         = var.container_definitions
-  cpu                           = var.cpu
+  alarms                         = var.alarms
+  assign_public_ip               = var.assign_public_ip
+  autoscaling_max_capacity       = var.autoscaling_max_capacity
+  autoscaling_min_capacity       = var.autoscaling_min_capacity
+  autoscaling_policies           = var.autoscaling_policies
+  autoscaling_scheduled_actions  = var.autoscaling_scheduled_actions
+  autoscaling_suspended_state    = var.autoscaling_suspended_state
+  availability_zone_rebalancing  = var.availability_zone_rebalancing
+  capacity_provider_strategy     = var.capacity_provider_strategy
+  cluster_arn                    = var.cluster_arn
+  container_definitions          = var.container_definitions
+  cpu                            = var.cpu
+  create_iam_role                = var.create_iam_role
+  create_infrastructure_iam_role = var.create_infrastructure_iam_role
+  create_security_group          = var.create_security_group
+  create_service                 = var.create_service
+  create_task_definition         = var.create_task_definition
+  create_task_exec_iam_role      = var.create_task_exec_iam_role
+  create_task_exec_policy        = var.create_task_exec_policy
+  create_tasks_iam_role          = var.create_tasks_iam_role
 }
