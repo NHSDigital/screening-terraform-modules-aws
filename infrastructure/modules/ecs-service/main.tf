b8551b25c46f9a062638f5e643453823ced89139
Dave Hinton <dave.hinton1@nhs.net>
2026-06-22 08:41:35 +0100

    feat(ecs-service): add inputs a

--- infrastructure/modules/ecs-service/main.tf
+++ infrastructure/modules/ecs-service/main.tf
@@ -5,4 +5,13 @@ module "ecs_service" {
   create = module.this.enabled
   name   = module.this.name
   tags   = module.this.tags
+
+  alarms                        = var.alarms
+  assign_public_ip              = var.assign_public_ip
+  autoscaling_max_capacity      = var.autoscaling_max_capacity
+  autoscaling_min_capacity      = var.autoscaling_min_capacity
+  autoscaling_policies          = var.autoscaling_policies
+  autoscaling_scheduled_actions = var.autoscaling_scheduled_actions
+  autoscaling_suspended_state   = var.autoscaling_suspended_state
+  availability_zone_rebalancing = var.availability_zone_rebalancing
 }
