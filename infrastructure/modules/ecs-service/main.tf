82d768195c2b97e8186334c113c9d108803ab319
Dave Hinton <dave.hinton1@nhs.net>
2026-06-22 08:20:32 +0100

    feat(ecs-service): add context

--- infrastructure/modules/ecs-service/main.tf
+++ infrastructure/modules/ecs-service/main.tf
@@ -1,4 +1,8 @@
 module "ecs_service" {
   source  = "terraform-aws-modules/ecs/aws//modules/service"
   version = "~> 7.5.0"
+
+  create = module.this.enabled
+  name   = module.this.name
+  tags   = module.this.tags
 }
