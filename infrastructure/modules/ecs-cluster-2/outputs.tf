e69608aace742d37723ca3991530d02c7b668e47
Oliver Slater <oliver.slater2@nhs.net>
2026-06-23 02:14:48 +0100

    feat(ecs-cluster): refactor ECS Cluster module from upstream
    - Introduced a new context.tf file to standardize tagging and naming conventions.
    - Removed ecs.tf and migrated ECS cluster resource definitions to main.tf using the community ECS cluster module.
    - Added locals.tf to derive cluster name and CloudWatch log group configurations.
    - Updated outputs.tf to reflect new output values from the ECS cluster module.
    - Removed the readme.md file as it was outdated and no longer needed.
    - Enhanced variables.tf with new variables for ECS cluster configuration, including CloudWatch log group settings and capacity providers.
    - Updated versions.tf to include the time provider and adjust the AWS provider version.
    
    BREAKING CHANGE: Removes original ecs-cluster module that had nested resources i.e. aws_security_group, aws_iam_service_linked_role, aws_sns_topic, aws_cloudwatch_metric_alarm.

--- infrastructure/modules/ecs-cluster-2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_arn" {
-  description = "ARN that identifies the cluster"
-  value       = module.ecs_cluster.arn
-}
-
-output "cluster_name" {
-  description = "Name that identifies the cluster"
-  value       = module.ecs_cluster.name
-}
