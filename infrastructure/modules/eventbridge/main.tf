4d8f182261c18f58de7b0d61ba399408fc2dcaea
Dave Hinton <dave.hinton1@nhs.net>
2026-09-16 08:17:27 +0100

    chore: add context

--- infrastructure/modules/eventbridge/main.tf
+++ infrastructure/modules/eventbridge/main.tf
@@ -1,5 +1,8 @@
 module "eventbridge" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-eventbridge.git?ref=f9934726324c988f823682884b4fa003586a7b6f" # v4.3.2
 
+  create = module.this.enabled
+  tags   = module.this.tags
+
   # DAVEH: add attributes
 }
