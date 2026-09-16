b57717b702ac42d9086de3fd905f6b157f6d41b3
Dave Hinton <dave.hinton1@nhs.net>
2026-09-16 11:42:26 +0100

    docs: todo

--- infrastructure/modules/eventbridge/main.tf
+++ infrastructure/modules/eventbridge/main.tf
@@ -1,6 +1,8 @@
 module "eventbridge" {
   source = "git::https://github.com/terraform-aws-modules/terraform-aws-eventbridge.git?ref=f9934726324c988f823682884b4fa003586a7b6f" # v4.3.2
 
+  # DAVEH: force enable encryption
+
   create = module.this.enabled
   tags   = module.this.tags
 
